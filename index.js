4edd84c15c02affd6d8a0ad46badab574355da4d
Manuel Castro <manuelcastro911@gmail.com>
2024-04-27 10:51:23 -0500

    arreglo de cors

--- index.js
+++ index.js
@@ -5,21 +5,18 @@ const { urlencoded, json } = require('express');
 const conexion = require('./db/dbmongo');
 require('dotenv').config();
 
-
 const app = express();
-const port = process.env.PORT
+const port = process.env.PORT;
 const prefix = '/v1';
 
 app.use(urlencoded({ extended: true }));
 app.use(json());
 
+// Configuración correcta de CORS
 app.use(cors({
-  origin: 'https://reservas-front.vercel.app/'
+  origin: 'https://reservas-front.vercel.app', // Elimina el slash al final
+  optionsSuccessStatus: 200 // Para navegadores antiguos que no pueden manejar códigos de estado predeterminados
 }));
-
-
-
-app.use(cors());
 
 // Crear un nuevo enrutador para todas las rutas en routerApi
 const reservasRouter = express.Router();
@@ -30,5 +27,4 @@ app.use(prefix, reservasRouter);
 
 app.listen(port, () => {
   console.log(`Listening at port ${port}`);
-
 });
